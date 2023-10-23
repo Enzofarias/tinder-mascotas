8690be045e975b46bb627cb5e200bd9d4e904fdd
Enzo Farias <fariasenzo38@gmail.com>
2023-10-23 13:37:17 -0300

    Primer commit

---
+++ src/test/java/egg/tindermascotas/TinderMascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package egg.tindermascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderMascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
